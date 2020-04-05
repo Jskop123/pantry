b3ffe6e4f473493c7ef30c30f0cc347a1ebb8f32
kuba <jskop123@gmail.com>
2020-04-05 13:34:52 +0200

    remove build from gitignore

---
+++ build/precache-manifest.8c8a6d3b6187fcf5bdb52111fb580016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd698ae9f2db29ff4985c0bacffaf394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4450189a50a8bd583f",
+    "url": "/static/css/main.5cbb668d.chunk.css"
+  },
+  {
+    "revision": "e415b75ad605d7f1f222",
+    "url": "/static/js/2.b7de3421.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b7de3421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4450189a50a8bd583f",
+    "url": "/static/js/main.286cf78b.chunk.js"
+  },
+  {
+    "revision": "d3719874c1da4913d269",
+    "url": "/static/js/runtime-main.92228de0.js"
+  }
+]);
